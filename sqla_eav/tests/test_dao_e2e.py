b16179357dc9fecbf497a30a44bae735abe91630
adorsk <adorsk@fas.harvard.edu>
2017-07-06 15:40:48 -0400

    prop => attr

--- sqla_eav/tests/test_dao_e2e.py
+++ sqla_eav/tests/test_dao_e2e.py
@@ -9,11 +9,11 @@ class BaseTestCase(unittest.TestCase):
         self.db_uri = 'sqlite://'
         self.dao = dao.Dao(db_uri=self.db_uri)
         self.dao.create_tables()
-        self.props = {'prop_%s' % i: 'value_%s' % i for i in range(3)}
+        self.attrs = {'attr_%s' % i: 'value_%s' % i for i in range(3)}
 
 class CreateEntTestCase(BaseTestCase):
     def test_create_ent(self):
-        ent = self.dao.create_ent(props=self.props)
+        ent = self.dao.create_ent(attrs=self.attrs)
         fetched_ents = self.dao.query_ents(query={
             'ent_filters': [
                 {'col': 'key', 'op': '=', 'arg': ent['key']}
@@ -24,14 +24,14 @@ class CreateEntTestCase(BaseTestCase):
 class PatchEntTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.ent = self.dao.create_ent(props=self.props)
+        self.ent = self.dao.create_ent(attrs=self.attrs)
         self.patches = self.generate_patches()
 
     def generate_patches(self):
         now = time.time()
         return {
             key: '{key}:new_value:{now}'.format(key=key, now=now)
-            for key in self.props
+            for key in self.attrs
         }
 
     def test_update_ent(self):
@@ -39,15 +39,15 @@ class PatchEntTestCase(BaseTestCase):
         self.assert_patches(patches=self.patches)
 
     def assert_patches(self, patches=None):
-        ent_prop_dicts = self.dao.execute_sql(sql=textwrap.dedent(
+        ent_attr_dicts = self.dao.execute_sql(sql=textwrap.dedent(
             '''
-            SELECT ents.modified as ent_modified, props.* FROM
-            ents JOIN props ON ents.key=props.ent_key
+            SELECT ents.modified as ent_modified, attrs.* FROM
+            ents JOIN attrs ON ents.key=attrs.ent_key
             '''
         ))
         self.assertEqual(
-            self.dao.ent_prop_dicts_to_ent_dicts(
-                ent_prop_dicts)[self.ent['key']]['props'],
+            self.dao.ent_attr_dicts_to_ent_dicts(
+                ent_attr_dicts)[self.ent['key']]['attrs'],
             patches
         )
 
@@ -64,34 +64,34 @@ class PatchEntTestCase(BaseTestCase):
         self.assert_patches(patches=patches_2)
 
 class QueryPropsTestCase(BaseTestCase):
-    def test_prop_existence_query(self):
-        self.dao.create_ent(props=self.props)
+    def test_attr_existence_query(self):
+        self.dao.create_ent(attrs=self.attrs)
         sql = textwrap.dedent(
             '''
-            SELECT props.* FROM ents JOIN props ON ents.key=props.ent_key
-            WHERE props.prop=:prop
+            SELECT attrs.* FROM ents JOIN attrs ON ents.key=attrs.ent_key
+            WHERE attrs.attr=:attr
             '''
         )
-        prop_dicts = self.dao.execute_sql(
-            sql, params={'prop': list(self.props.keys())[0]})
-        self.assertTrue(len(prop_dicts) > 0)
+        attr_dicts = self.dao.execute_sql(
+            sql, params={'attr': list(self.attrs.keys())[0]})
+        self.assertTrue(len(attr_dicts) > 0)
 
 class CastPropsTestCase(BaseTestCase):
-    def test_prop_casting(self):
+    def test_attr_casting(self):
         idxs = list(range(6))
-        for idx in idxs: self.dao.create_ent(props={'idx': idx})
+        for idx in idxs: self.dao.create_ent(attrs={'idx': idx})
         sql = textwrap.dedent(
             '''
-            SELECT prop, value
-            FROM props
+            SELECT attr, value
+            FROM attrs
             WHERE
-                prop=:prop
+                attr=:attr
                 AND (CAST(value AS INT) % 2) =:value
             '''
         )
-        prop_dicts = self.dao.execute_sql(sql, params={'prop': 'idx',
+        attr_dicts = self.dao.execute_sql(sql, params={'attr': 'idx',
                                                        'value': 0})
-        self.assertEqual(prop_dicts, [{'prop': 'idx', 'value': str(idx)} 
+        self.assertEqual(attr_dicts, [{'attr': 'idx', 'value': str(idx)} 
                                       for idx in idxs if (idx % 2) == 0])
 
 class QueryEntsTestCase(BaseTestCase):
@@ -112,63 +112,63 @@ class QueryEntsTestCase(BaseTestCase):
             }
         }
         self.ents = {
-            ent_key: self.dao.create_ent(ent_key=ent_key, props=ent_props)
-            for ent_key, ent_props in self.ent_specs.items()
+            ent_key: self.dao.create_ent(ent_key=ent_key, attrs=ent_attrs)
+            for ent_key, ent_attrs in self.ent_specs.items()
         }
 
-    def test_limit_props(self):
-        props_to_select = ['a', 'b']
+    def test_limit_attrs(self):
+        attrs_to_select = ['a', 'b']
         actual = self.dao.query_ents(query={
-            'props_to_select': props_to_select
+            'attrs_to_select': attrs_to_select
         })
         expected = {
             ent_key: {
                 'modified': self.ents[ent_key]['modified'],
                 'key': ent_key,
-                'props': {p: k for p, k in ent['props'].items()
-                          if p in props_to_select}
+                'attrs': {p: k for p, k in ent['attrs'].items()
+                          if p in attrs_to_select}
             }
             for ent_key, ent in self.ents.items()
         }
         self.assertEqual(actual, expected)
 
-    def test_prop_binary_filters(self):
-        prop_filters = [
-            {'prop': 'a', 'op': '=', 'arg': 'a.1'},
-            {'prop': 'b', 'op': '=', 'arg': 'b.2'},
+    def test_attr_binary_filters(self):
+        attr_filters = [
+            {'attr': 'a', 'op': '=', 'arg': 'a.1'},
+            {'attr': 'b', 'op': '=', 'arg': 'b.2'},
         ]
-        actual = self.dao.query_ents(query={'prop_filters': prop_filters})
+        actual = self.dao.query_ents(query={'attr_filters': attr_filters})
         expected = {}
         for ent_key, ent in self.ents.items():
             passed = True
-            for filter_ in prop_filters:
-                if ent['props'].get(filter_['prop']) != filter_['arg']:
+            for filter_ in attr_filters:
+                if ent['attrs'].get(filter_['attr']) != filter_['arg']:
                     passed = False
                     break
             if passed: expected[ent_key] = ent
         self.assertEqual(actual, expected)
 
-    def test_prop_exists_filters(self):
-        prop_filters = [
-            {'prop': 'c', 'op': 'EXISTS'}
+    def test_attr_exists_filters(self):
+        attr_filters = [
+            {'attr': 'c', 'op': 'EXISTS'}
         ]
-        actual = self.dao.query_ents(query={'prop_filters': prop_filters})
+        actual = self.dao.query_ents(query={'attr_filters': attr_filters})
         expected = {
             ent_key: ent
             for ent_key, ent in self.ents.items()
-            if 'c' in ent['props']
+            if 'c' in ent['attrs']
         }
         self.assertEqual(actual, expected)
 
-    def test_prop_not_exists_filters(self):
-        prop_filters = [
-            {'prop': 'c', 'op': '! EXISTS'}
+    def test_attr_not_exists_filters(self):
+        attr_filters = [
+            {'attr': 'c', 'op': '! EXISTS'}
         ]
-        actual = self.dao.query_ents(query={'prop_filters': prop_filters})
+        actual = self.dao.query_ents(query={'attr_filters': attr_filters})
         expected = {
             ent_key: ent
             for ent_key, ent in self.ents.items()
-            if 'c' not in ent['props']
+            if 'c' not in ent['attrs']
         }
         self.assertEqual(actual, expected)
 
@@ -178,17 +178,17 @@ class UpsertEntTestCase(BaseTestCase):
         self.ent_key = 'my_ent_key'
 
     def test_creates_if_not_exists(self):
-        self.dao.upsert_ent(ent_key=self.ent_key, patches=self.props)
+        self.dao.upsert_ent(ent_key=self.ent_key, patches=self.attrs)
         fetched_ents = self.dao.query_ents()
-        self.assertEqual(fetched_ents[self.ent_key]['props'], self.props)
+        self.assertEqual(fetched_ents[self.ent_key]['attrs'], self.attrs)
 
     def test_patches_if_exists(self):
-        self.dao.create_ent(ent_key=self.ent_key, props=self.props)
-        prop_keys = list(self.props.keys())
-        patches = {prop: '{prop}_new_value'.format(prop=prop)
-                   for prop in prop_keys[:-1]}
-        deletions = [prop_keys[-1]]
+        self.dao.create_ent(ent_key=self.ent_key, attrs=self.attrs)
+        attr_keys = list(self.attrs.keys())
+        patches = {attr: '{attr}_new_value'.format(attr=attr)
+                   for attr in attr_keys[:-1]}
+        deletions = [attr_keys[-1]]
         self.dao.upsert_ent(ent_key=self.ent_key, patches=patches,
                             deletions=deletions)
         fetched_ents = self.dao.query_ents()
-        self.assertEqual(fetched_ents[self.ent_key]['props'], patches)
+        self.assertEqual(fetched_ents[self.ent_key]['attrs'], patches)
